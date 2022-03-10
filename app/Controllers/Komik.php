632ba72f274056c75bdb652f2a50df6835eac2c3
bagoestirto <bagoestirto@gmail.com>
2022-03-10 14:43:11 +0700

    Menambahkan model

---
+++ app/Controllers/Komik.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Models\KomikModel;
+
+class Komik extends BaseController
+{
+    protected $komikModel;
+
+    public function __construct()
+    {
+        $this->komikModel = new KomikModel();
+    }
+    public function index()
+    {
+        $komik = $this->komikModel->findAll();
+        $data = [
+            'tittle' => "Daftar Komik",
+            'komik' => $komik
+        ];
+
+        //cara konek db tanpa model
+        // $db = \Config\Database::connect();
+        // $komik = $db->query("SELECT * FROM komik");
+        // foreach ($komik->getResultArray() as $row) {
+        //     d($row);
+        // }
+
+
+
+
+        return view('komik/index', $data);
+    }
+}
--- app/Views/layout/navbar.php
+++ app/Views/layout/navbar.php
@@ -16,6 +16,9 @@
                     <li class="nav-item">
                         <a class="nav-link" href="<?= base_url('/pages/contact'); ?>">Contact</a>
                     </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="<?= base_url('/komik'); ?>">Komik</a>
+                    </li>
                     <!-- <li class="nav-item dropdown">
                             <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                                 Dropdown
